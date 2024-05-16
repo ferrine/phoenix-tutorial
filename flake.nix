7fbd294a832f6a54eba6eb9b3e2f669de1b7b56c
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-05-16 13:50:26 +0300

    setup env for running code

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root = {
+      url = "github:srid/flake-root";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, flake-root, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nix
+	flake-root.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     };

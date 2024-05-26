77bfad3bd74b40ed8e1d6d6134d92052dead91b9
ferres <justferres@yandex.ru>
2024-05-26 22:11:44 +0300

    add some comments

--- src/pento/lib/pento_web/live/product_live/index.ex
+++ src/pento/lib/pento_web/live/product_live/index.ex
@@ -1,4 +1,17 @@
 defmodule PentoWeb.ProductLive.Index do
+  # for the live_module action
+  # in the book navigate was described instead.
+  # this change was introduced in
+  # https://github.com/phoenixframework/phoenix/commit/47b8ac061f4ddca5ae9d0cc7ed9a1c590f338edb#diff-98799d23ca76694dec61fdb11327dc117ae19084d3b5150805204df06a0c05d2R24
+  # on Feb 14, 2023
+  # and enabled the possiblity to not call mount/3 function on the parent view. So minimal updates are propagated.
+  # previously, the default table component had a naive implementation with list of possible rows.
+  # In that same PR the change was introduced to that core component that is very interesting
+  # https://github.com/phoenixframework/phoenix/commit/47b8ac061f4ddca5ae9d0cc7ed9a1c590f338edb#diff-d84568fb24f951fcecda1f4c6de916e4c0899869e702b350d10237ba0dc30c81R470
+  # this adds a function argument to the table component to modify the row if needed
+  #
+  # https://github.com/phoenixframework/phoenix/commit/47b8ac061f4ddca5ae9d0cc7ed9a1c590f338edb#diff-b6c55a2ac979fa384a633e8f7dc2400555f672cba24f3f3269cc4cba08c559c2R495
+  # will make sure to only use the update method on live stream rows
   use PentoWeb, :live_view
 
   alias Pento.Catalog

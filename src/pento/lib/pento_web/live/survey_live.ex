e26b421de681fbb6431a9f92fcfe545ab99b1506
ferres <justferres@yandex.ru>
2024-05-30 19:35:00 +0300

    add the survey page and render the demographic content.
    Note, I did not like the table display of the demographic and skipped that

--- src/pento/lib/pento_web/live/survey_live.ex
+++ src/pento/lib/pento_web/live/survey_live.ex
@@ -1,7 +1,20 @@
 defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
+  alias Pento.Survey
+  alias PentoWeb.DemographicLive
+  alias __MODULE__.Component
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {
+      :ok,
+      socket
+      |> assign_demographic()
+    }
+  end
+
+  defp assign_demographic(socket) do
+    user = socket.assigns.current_user
+    socket
+    |> assign(:demographic, Survey.get_demographic_by_user(user))
   end
 end

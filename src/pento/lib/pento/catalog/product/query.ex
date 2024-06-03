e551c4d60f03bdc28b6727fa0b717cde63ccc262
ferres <justferres@yandex.ru>
2024-06-03 20:06:41 +0300

    add gender filters

--- src/pento/lib/pento/catalog/product/query.ex
+++ src/pento/lib/pento/catalog/product/query.ex
@@ -47,6 +47,21 @@ defmodule Pento.Catalog.Product.Query do
     |> join(:left, [p, r, u, d], d in Demographic, on: d.user_id == u.id)
   end
 
+  def filter_by_gender(query \\ base(), filter) do
+    query
+    |> apply_gender_filter(filter)
+  end
+
+  defp apply_gender_filter(query, "all") do
+    query
+  end
+
+  defp apply_gender_filter(query, gender)
+       when is_binary(gender) and gender in ["male", "female", "prefer not to say"] do
+    query
+    |> where([p, r, u, d], d.gender == ^gender)
+  end
+
   def filter_by_age_group(query \\ base(), filter) do
     query
     |> apply_age_group_filter(filter)

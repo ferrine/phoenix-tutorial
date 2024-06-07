1ecac1b47330de9fad65930f40bb069e0b46ac79
ferres <justferres@yandex.ru>
2024-06-07 15:47:36 +0300

    implement live component on the game view

--- src/pento/lib/pento_web/live/game_live/game_live.ex
+++ src/pento/lib/pento_web/live/game_live/game_live.ex
@@ -1,7 +1,8 @@
 defmodule PentoWeb.GameLive do
   use PentoWeb, :live_view
+  alias PentoWeb.GameLive.Board
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(%{"puzzle" => puzzle}, _session, socket) do
+    {:ok, assign(socket, :puzzle, puzzle)}
   end
 end
